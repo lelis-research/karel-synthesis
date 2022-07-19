f877b05bda8b2365e346cbf648ffbc35d28c70c9
Tales Carvalho <tales-h-carvalho@hotmail.com>
2022-07-19 01:36:21 -0600

    Class for training and environment config

---
+++ embedding/config/config.py
@@ -0,0 +1,12 @@
+from typing import NamedTuple
+
+
+class Config(NamedTuple):
+    device: str = 'cpu'
+    hidden_size: int = 64
+    max_program_len: int = 45
+    num_demo_per_program: int = 10
+    env_height: int = 8
+    env_width: int = 8
+    max_demo_length: int = 100
+    use_teacher_enforcing: bool = True
