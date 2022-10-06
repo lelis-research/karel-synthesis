5933b42cf37de50b368cdf00e08dc5f23b553fb4
Tales Carvalho <tales-h-carvalho@hotmail.com>
2022-10-05 18:14:54 -0600

    Updated and changed to npz data

--- experiment/train.py
+++ experiment/train.py
@@ -1,17 +1,15 @@
-import numpy as np
 import sys
 import torch
 import torch.optim as optim
 import torch.nn as nn
 from torch.utils.data import DataLoader, random_split
-import tqdm
 
 sys.path.insert(0, '.')
 
 from experiment.data_loader import ProgramDataset
 from experiment.model import StatePredictor
 
-N_EPOCH = 50
+N_EPOCH = 250
 TRAIN_SPLIT = 0.8
 VAL_SPLIT = 0.1
 BATCH_SIZE = 1024
@@ -21,7 +19,7 @@ if __name__ == '__main__':
 
     device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 
-    dataset = ProgramDataset('data/experiment.hdf5')
+    dataset = ProgramDataset('data/experiment.npz')
 
     # Train / Val / Test split
     train_len = int(TRAIN_SPLIT * len(dataset))
@@ -42,57 +40,82 @@ if __name__ == '__main__':
     criterion = nn.CrossEntropyLoss()
 
     # Training
-    min_val_loss = np.inf
+    train_losses = []
+    train_accs = []
+    val_losses = []
+    val_accs = []
+
+    max_val_accuracy = 0
     for epoch in range(N_EPOCH):
 
         model.train()
-        train_loss = 0
-        train_accuracy = 0
+        train_loss = 0.
+        train_accuracy = 0.
+        train_total = 0
 
-        with tqdm.tqdm(train_dl, unit='batch', desc=f'Epoch {epoch} Training') as train_batch:
+        print(f'Epoch {epoch}: Starting training...')
 
-            for i, data in enumerate(train_batch):
+        for i, data in enumerate(train_dl):
 
-                z, s_s, s_f = (d.to(device) for d in data)
+            z, s_s, s_f = (d.to(device) for d in data)
 
-                optimizer.zero_grad()
+            optimizer.zero_grad()
 
-                output = model(s_s, z)
-                loss = criterion(output, s_f)
-                loss.backward()
-                optimizer.step()
+            output = model(s_s, z)
+            loss = criterion(output, s_f)
+            loss.backward()
+            optimizer.step()
 
-                train_loss += loss.item()
+            train_loss += loss.item()
 
-                train_batch.set_postfix(loss=loss.item())
+            predicton_output = torch.argmax(output, dim=1)
+            train_accuracy += (predicton_output == s_f).float().sum()
 
-                train_accuracy += (output == s_f)
+            train_total += z.size(0)
         
-        print(f'Epoch {epoch} training loss: {train_loss / len(train_dl)}')
+        print(f'Epoch {epoch} training loss: {train_loss / train_total}')
+        print(f'Epoch {epoch} training acc: {train_accuracy / train_total}')
+
+        train_losses.append(train_loss / train_total)
+        train_accs.append(train_accuracy / train_total)
 
         model.eval()
-        val_loss = 0.0
+        val_loss = 0.
+        val_accuracy = 0.
+        val_total = 0
 
-        with tqdm.tqdm(val_dl, unit='batch', desc=f'Epoch {epoch} Validation') as val_batch:
+        for i, data in enumerate(val_dl):
 
-            for i, data in enumerate(val_batch):
+            z, s_s, s_f = (d.to(device) for d in data)
 
-                z, s_s, s_f = (d.to(device) for d in data)
+            output = model(s_s, z)
+            loss = criterion(output, s_f)
 
-                # TODO: accuracy?
+            val_loss += loss.item()
 
-                output = model(s_s, z)
-                loss = criterion(output, s_f)
+            predicton_output = torch.argmax(output, dim=1)
+            val_accuracy += (predicton_output == s_f).float().sum()
 
-                val_loss += loss.item()
+            val_total += z.size(0)
+    
+        print(f'Epoch {epoch} validation loss: {val_loss / val_total}')
+        print(f'Epoch {epoch} validation acc: {val_accuracy / val_total}')
 
-                val_batch.set_postfix(loss=loss.item())
-        
-        print(f'Epoch {epoch} validation loss: {val_loss / len(val_dl)}')
+        val_losses.append(val_loss / val_total)
+        val_accs.append(val_accuracy / val_total)
 
-        if min_val_loss > val_loss:
+        if max_val_accuracy < val_accuracy:
 
-            min_val_loss = val_loss
+            max_val_accuracy = val_accuracy
+
+            print('New best validation accuracy, saving model...')
 
             # Save parameters if new best validation
-            torch.save(model.state_dict(), 'data/experiment_model.pth')
+            torch.save(model.state_dict(), 'output/experiment_model.pth')
+
+    with open('output/experiment_training_data.csv', mode='w') as f:
+        f.write('train_loss,train_acc,val_loss,val_acc')
+        for train_loss, train_acc, val_loss, val_acc in zip(
+            train_losses, train_accs, val_losses, val_accs
+        ):
+            f.write(f'\n{train_loss},{train_acc},{val_loss},{val_acc}')
