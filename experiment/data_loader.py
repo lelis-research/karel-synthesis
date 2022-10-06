54625e79fcf35de66dbef41835010a7a1c22947d
Tales Carvalho <tales-h-carvalho@hotmail.com>
2022-10-05 18:14:44 -0600

    Changed to npz data

--- experiment/data_loader.py
+++ experiment/data_loader.py
@@ -3,22 +3,22 @@ import numpy as np
 import torch
 from torch.utils.data import Dataset
 
-
 class ProgramDataset(Dataset):
 
     def __init__(self, data_path: str):
-        self.data_path = data_path
+        data = np.load(data_path)
+        self.data_z = torch.tensor(data['z'], dtype=torch.float)
+        self.data_s_s = torch.tensor(data['s_s'], dtype=torch.float)
+        self.data_s_s = torch.moveaxis(self.data_s_s, [-1,-2,-3], [-3,-1,-2])
+        self.data_s_f = torch.tensor(data['s_f'], dtype=torch.float)
+        self.data_s_f = self.data_s_f[:,:,:,0:4]
+        self.data_s_f = torch.sum(self.data_s_f, dim=3)
+        self.data_s_f = torch.flatten(self.data_s_f, start_dim=1)
+        self.data_s_f = torch.argmax(self.data_s_f, dim=1)
+        pass
 
     def __getitem__(self, idx):
-        with h5py.File(self.data_path, 'r') as hf:
-            z = hf[f'{idx:06d}']['z'][:]
-            s_s = hf[f'{idx:06d}']['s_s'][:]
-            s_f = np.sum(hf[f'{idx:06d}']['s_f'][:,:,0:4], axis=-1)
-            z = torch.tensor(z, dtype=torch.float)
-            s_s = torch.moveaxis(torch.tensor(s_s, dtype=torch.float), [-1,-2,-3], [-3,-1,-2])
-            s_f = torch.flatten(torch.tensor(s_f, dtype=torch.float))
-            return z, s_s, s_f
+        return self.data_z[idx], self.data_s_s[idx], self.data_s_f[idx]
 
     def __len__(self):
-        with h5py.File(self.data_path, 'r') as hf:
-            return len(hf.keys())
+        return len(self.data_z)
