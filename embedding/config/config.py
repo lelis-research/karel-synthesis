47d6eed24ce93b86337d1e9ede984d27100280a3
Tales Carvalho <tales-h-carvalho@hotmail.com>
2023-03-06 11:00:20 -0700

    Changed default hidden size to 256

--- embedding/config/config.py
+++ embedding/config/config.py
@@ -3,7 +3,7 @@ from typing import NamedTuple
 
 class Config(NamedTuple):
     batch_size: int = 256
-    hidden_size: int = 64
+    hidden_size: int = 256
     max_program_len: int = 45
     num_demo_per_program: int = 10
     env_height: int = 8
