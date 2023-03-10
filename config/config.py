6e638b0bde1d55a97850f70b8cfd0981c291f6bd
Tales Carvalho <tales-h-carvalho@hotmail.com>
2023-03-10 15:35:00 -0700

    Config parsing

--- config/config.py
+++ config/config.py
@@ -1,48 +1,56 @@
-class Config:
+from typing import Annotated, get_type_hints
 
-    model_name: str = 'program_vae'
-    model_hidden_size: int = 256
+class Config:
+    """Class that handles the project global configuration.
+    """
+
+    model_name: Annotated[str, 'Name of the model, used for saving output.'] = 'program_vae'
+    model_hidden_size: Annotated[int, 'Number of dimensions in hidden unit.'] = 256
     
-    data_batch_size: int = 256
-    data_max_program_len: int = 45
-    data_max_demo_length: int = 100
-    data_num_demo_per_program: int = 10
-    data_ratio_train: float = 0.7
-    data_ratio_val: float = 0.15
-    data_ratio_test: float = 0.15
+    data_batch_size: Annotated[int, ''] = 256
+    data_max_program_len: Annotated[int, ''] = 45
+    data_max_demo_length: Annotated[int, ''] = 100
+    data_num_demo_per_program: Annotated[int, ''] = 10
+    data_ratio_train: Annotated[float, ''] = 0.7
+    data_ratio_val: Annotated[float, ''] = 0.15
+    data_ratio_test: Annotated[float, ''] = 0.15
     
-    env_task: str = 'StairClimber'
-    env_seed: int = 1
-    env_height: int = 8
-    env_width: int = 8
-    env_leaps_behaviour: bool = False
-    env_crashable: bool = True
+    env_task: Annotated[str, ''] = 'StairClimber'
+    env_seed: Annotated[int, ''] = 1
+    env_height: Annotated[int, ''] = 8
+    env_width: Annotated[int, ''] = 8
+    env_leaps_behaviour: Annotated[bool, ''] = False
+    env_crashable: Annotated[bool, ''] = True
     
-    search_elitism_rate: float = 0.1
-    search_population_size: int = 256
-    search_sigma: float = 0.2
-    search_number_executions: int = 100
-    search_number_iterations: int = 1000
+    search_elitism_rate: Annotated[float, ''] = 0.1
+    search_population_size: Annotated[int, ''] = 256
+    search_sigma: Annotated[float, ''] = 0.2
+    search_number_executions: Annotated[int, ''] = 100
+    search_number_iterations: Annotated[int, ''] = 1000
     
-    trainer_num_epochs: int = 150
-    trainer_prog_teacher_enforcing: bool = True
-    trainer_a_h_teacher_enforcing: bool = True
-    trainer_prog_loss_coef: float = 1.0
-    trainer_a_h_loss_coef: float = 1.0
-    trainer_latent_loss_coef: float = 0.1
-    trainer_optim_lr: float = 5e-4
+    trainer_num_epochs: Annotated[int, ''] = 150
+    trainer_prog_teacher_enforcing: Annotated[bool, ''] = True
+    trainer_a_h_teacher_enforcing: Annotated[bool, ''] = True
+    trainer_prog_loss_coef: Annotated[float, ''] = 1.0
+    trainer_a_h_loss_coef: Annotated[float, ''] = 1.0
+    trainer_latent_loss_coef: Annotated[float, ''] = 0.1
+    trainer_optim_lr: Annotated[float, ''] = 5e-4
     
     @classmethod
     def parse_args(cls):
         """Generates an argparser for setting all Config attributes through command-line.
         """        
-        from argparse import ArgumentParser
+        from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter
         
-        parser = ArgumentParser()
+        parser = ArgumentParser(formatter_class=ArgumentDefaultsHelpFormatter)
+        hints = get_type_hints(cls, include_extras=True)
         for param_name in cls.__annotations__:
+            param_hints = hints.get(param_name).__dict__.get('__metadata__')
+            joined_hints = ', '.join(param_hints) if param_hints else ''
             parser.add_argument(f'--{param_name}',
                                 default=cls.__dict__[param_name],
-                                type=cls.__annotations__[param_name])
+                                type=cls.__annotations__[param_name],
+                                help=joined_hints)
         
         args_dict = vars(parser.parse_args())
         
