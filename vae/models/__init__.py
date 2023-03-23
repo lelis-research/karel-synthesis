55ed354032cd6049e3d0a798d3c3868f47d5c76a
Tales Carvalho <tales-h-carvalho@hotmail.com>
2023-03-23 00:14:50 -0300

    Double VAE Translator MLP Module

--- vae/models/__init__.py
+++ vae/models/__init__.py
@@ -3,6 +3,7 @@ from .leaps_vae import LeapsVAE
 from .policy_vae import PolicyVAE
 from .sketch_vae import SketchVAE
 from .double_vae import DoubleVAE
+from .double_vae_translator import DoubleVAETranslator
 
 from dsl import DSL
 import torch
