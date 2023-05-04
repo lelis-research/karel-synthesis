aa8477109ae5a26358dd4386dd3c830b0e4c3af7
Tales Carvalho <tales-h-carvalho@hotmail.com>
2023-05-04 17:27:49 -0600

    Implemented DFS and MAB versions of HS

---
+++ search/hierarchical_search.py
@@ -0,0 +1,213 @@
+from __future__ import annotations
+from functools import partial
+from multiprocessing import Pool
+import os
+import time
+import numpy as np
+import torch
+
+from dsl import DSL
+from dsl.base import Program
+from vae.models.base_vae import BaseVAE
+from vae.models.sketch_vae import SketchVAE
+from logger.stdout_logger import StdoutLogger
+from tasks.task import Task
+from config import Config
+
+def evaluate_program(program_tokens: list[int], task_envs: list[Task], dsl: DSL) -> float:
+    if program_tokens is None: return -float('inf')
+    
+    try:
+        program = dsl.parse_int_to_node(program_tokens)
+    except AssertionError: # In case of invalid program (e.g. does not have an ending token)
+        return -float('inf')
+        
+    rewards = [task_env.evaluate_program(program) for task_env in task_envs]
+    
+    return np.mean(rewards)
+
+
+class HierarchicalSearch:
+    
+    def __init__(self, models: list[BaseVAE], task_cls: type[Task], dsl: DSL):
+        self.models: list[BaseVAE] = models
+        self.iterations = [Config.hierarchical_level_1_iterations, Config.hierarchical_level_2_iterations]
+        self.pop_sizes = [Config.hierarchical_level_1_pop_size, Config.hierarchical_level_2_pop_size]
+        self.n_elite = [
+            int(Config.search_elitism_rate * self.pop_sizes[0]),
+            int(Config.search_elitism_rate * self.pop_sizes[1])
+        ]
+        self.dsl = dsl
+        self.sketch_dsl = dsl.extend_dsl()
+        self.device = self.models[0].device
+
+        self.number_executions = Config.search_number_executions
+        self.number_iterations = Config.search_number_iterations
+        self.sigma = Config.search_sigma
+        self.task_envs = [task_cls(i) for i in range(self.number_executions)]
+        output_dir = os.path.join('output', Config.experiment_name, 'latent_search')
+        os.makedirs(output_dir, exist_ok=True)
+        self.output_file = os.path.join(output_dir, f'seed_{Config.model_seed}.csv')
+        self.trace_file = os.path.join(output_dir, f'seed_{Config.model_seed}.gif')
+        self.restart_timeout = Config.search_restart_timeout
+
+
+    def evaluate_program(self, program_tokens: list[int]) -> float:
+        if program_tokens is None: return -float('inf')
+
+        try:
+            program = self.dsl.parse_int_to_node(program_tokens)
+        except AssertionError: # In case of invalid program (e.g. does not have an ending token)
+            return -float('inf')
+        
+        sum_reward = 0.
+        for task_env in self.task_envs:
+            reward = task_env.evaluate_program(program)
+            if reward < self.best_reward:
+                return -float('inf')
+            sum_reward += reward
+        
+        return sum_reward / len(self.task_envs)
+
+
+    def get_program(self, sketch_tokens: list[int], holes_tokens: list[list[int]]) -> list[int]:
+        # split sketch_tokens into multiple lists based on <HOLE> token
+        list_sketch = [[]]
+        for token in sketch_tokens:
+            if token == self.sketch_dsl.t2i['<HOLE>']:
+                list_sketch.append([])
+            else:
+                list_sketch[-1].append(token)
+        assert len(list_sketch) == len(holes_tokens) + 1
+        prog_tokens = list_sketch[0]
+        for i in range(len(holes_tokens)):
+            assert holes_tokens[i][0] == self.dsl.t2i['DEF']
+            assert holes_tokens[i][1] == self.dsl.t2i['run']
+            assert holes_tokens[i][2] == self.dsl.t2i['m(']
+            assert holes_tokens[i][-1] == self.dsl.t2i['m)']
+            prog_tokens += holes_tokens[i][3:-1] + list_sketch[i+1]
+        return prog_tokens
+
+
+    def recursive_search(self, level: int, current_program: list[int]):
+        n_holes = current_program.count(self.sketch_dsl.t2i['<HOLE>'])
+    
+        population = torch.stack([
+            torch.stack([
+                torch.randn(self.models[level].hidden_size, device=self.device)
+                for _ in range(n_holes)
+            ])
+            for _ in range(self.pop_sizes[level])
+        ])
+        best_program = None
+        best_reward = -float('inf')
+        prev_mean_elite_reward = -float('inf')
+        
+        for iteration in range(1, self.iterations[level] + 1):
+            StdoutLogger.log('Hierarchical Search', f'Level {level} Iteration {iteration}.')
+            population_holes_tokens = [
+                [self.models[level].decode_vector(hole.unsqueeze(0))[0] for hole in p]
+                for p in population
+            ]
+            
+            filled_programs = []
+            for holes_tokens in population_holes_tokens:
+                try:
+                    filled_program = self.get_program(current_program, holes_tokens)
+                    filled_programs.append(filled_program)
+                except AssertionError:
+                    filled_programs.append(None)
+
+            # If the program is still incomplete, we call this function recursively
+            if type(self.models[level]) == SketchVAE:
+                rewards = []
+                completed_programs = []
+                for filled_program in filled_programs:
+                    completed_program, reward = self.recursive_search(level + 1, filled_program)
+                    rewards.append(reward)
+                    completed_programs.append(completed_program)
+            # In case it is the last level of hierarchical search
+            else:
+                completed_programs = filled_programs
+                if Config.multiprocessing_active:
+                    with Pool() as pool:
+                        fn = partial(evaluate_program, task_envs=self.task_envs, dsl=self.dsl)
+                        rewards = pool.map(fn, completed_programs)
+                else:
+                    rewards = [evaluate_program(p, self.task_envs, self.dsl) for p in completed_programs]
+                self.num_eval += len(rewards)
+            
+            rewards = torch.tensor(rewards)
+            
+            best_indices = torch.topk(rewards, self.n_elite[level]).indices
+            elite_population = population[best_indices]
+            mean_elite_reward = torch.mean(rewards[best_indices])
+            
+            if mean_elite_reward.cpu().numpy() == prev_mean_elite_reward:
+                counter_for_restart += 1
+            else:
+                counter_for_restart = 0
+                
+            if counter_for_restart >= self.restart_timeout and self.restart_timeout > 0:
+                StdoutLogger.log('Hierarchical Search', f'Skipping level {level} search.')
+                break
+            
+            if torch.max(rewards) > best_reward:
+                best_reward = torch.max(rewards)
+                best_program = completed_programs[torch.argmax(rewards)]
+                
+            if best_reward > self.best_reward:
+                self.best_reward = best_reward
+                self.best_program = self.dsl.parse_int_to_str(best_program)
+                StdoutLogger.log('Hierarchical Search', f'New best program: {self.best_program}')
+                StdoutLogger.log('Hierarchical Search', f'New best reward: {self.best_reward}')
+                StdoutLogger.log('Hierarchical Search', f'Number of evaluations: {self.num_eval}')
+                with open(self.output_file, mode='a') as f:
+                    t = time.time() - self.start_time
+                    f.write(f'{t},{self.num_eval},{self.best_reward},{self.best_program}\n')
+            
+            if self.best_reward >= 1.0:
+                self.converged = True
+                break
+            
+            StdoutLogger.log('Hierarchical Search', f'Mean Elite Reward: {mean_elite_reward}')
+            StdoutLogger.log('Hierarchical Search', f'Num eval so far: {self.num_eval}')
+            
+            new_indices = torch.ones(elite_population.size(0), device=self.device).multinomial(
+                self.pop_sizes[level], replacement=True)
+            new_population = []
+            for index in new_indices:
+                sample = elite_population[index]
+                new_population.append(
+                    sample + self.sigma * torch.randn_like(sample, device=self.device)
+                )
+            population = torch.stack(new_population)
+            prev_mean_elite_reward = mean_elite_reward.cpu().numpy()
+        return best_program, best_reward
+
+    
+    def search(self) -> tuple[str, bool, int]:
+        self.converged = False
+        self.num_eval = 0
+        self.best_reward = float('-inf')
+        self.best_program = None
+        self.start_time = time.time()
+        with open(self.output_file, mode='w') as f:
+            f.write('iteration,time,num_evaluations,best_reward,best_program\n')
+        
+        for i in range(1, self.number_iterations + 1):
+            program = self.sketch_dsl.parse_str_to_int('DEF run m( <HOLE> m)')
+            best_program, best_reward = self.recursive_search(0, program)
+            best_program_str = self.dsl.parse_int_to_str(best_program)
+            
+            if self.converged: break
+            
+            StdoutLogger.log('Hierarchical Search',f'Iteration {i}:')
+            StdoutLogger.log('Hierarchical Search',f'Best reward: {best_reward}')
+            StdoutLogger.log('Hierarchical Search',f'Best program: {best_program_str}')
+            StdoutLogger.log('Hierarchical Search',f'Number of evaluations: {self.num_eval}')
+
+        best_program_nodes = self.dsl.parse_str_to_node(self.best_program)
+        self.task_envs[0].trace_program(best_program_nodes, self.trace_file, 1000)
+
+        return self.best_program, self.converged, self.num_eval
---
+++ search/hierarchical_search_mab.py
@@ -0,0 +1,260 @@
+from __future__ import annotations
+from functools import partial
+from multiprocessing import Pool
+import os
+import time
+import numpy as np
+import torch
+
+from dsl import DSL
+from vae.models.base_vae import BaseVAE
+from vae.models.sketch_vae import SketchVAE
+from logger.stdout_logger import StdoutLogger
+from tasks.task import Task
+from config import Config
+
+
+def evaluate_program(program_tokens: list[int], task_envs: list[Task], dsl: DSL) -> float:
+    try:
+        program = dsl.parse_int_to_node(program_tokens)
+    except AssertionError: # In case of invalid program (e.g. does not have an ending token)
+        return -float('inf')
+    
+    sum_reward = 0.
+    for task_env in task_envs:
+        sum_reward += task_env.evaluate_program(program)
+    
+    return sum_reward / len(task_envs)
+
+
+class HierarchicalSearchMAB:
+    
+    def __init__(self, models: list[BaseVAE], task_cls: type[Task], dsl: DSL, seed: int = None):
+        self.models: list[BaseVAE] = models
+        self.search_iterations = Config.hierarchical_mab_search_iterations
+        self.sample_iterations = Config.hierarchical_mab_sample_iterations
+        self.pop_sizes = [Config.hierarchical_level_1_pop_size, Config.hierarchical_level_2_pop_size]
+        self.n_elite = [
+            int(Config.search_elitism_rate * self.pop_sizes[0]),
+            int(Config.search_elitism_rate * self.pop_sizes[1])
+        ]
+        self.dsl = dsl.extend_dsl()
+        self.device = self.models[0].device
+        self.batch_size = Config.hierarchical_mab_batch_size
+
+        self.number_executions = Config.search_number_executions
+        self.sigma = Config.search_sigma
+        self.epsilon = Config.hierarchical_mab_epsilon
+        self.task_envs = [task_cls(i) for i in range(self.number_executions)]
+        output_dir = os.path.join('output', Config.experiment_name, 'latent_search')
+        os.makedirs(output_dir, exist_ok=True)
+        self.output_file = os.path.join(output_dir, f'seed_{Config.model_seed}.csv')
+        self.trace_file = os.path.join(output_dir, f'seed_{Config.model_seed}.gif')
+        self.restart_timeout = Config.search_restart_timeout
+        if seed is None:
+            self.rng = np.random.default_rng(Config.env_seed)
+        else:
+            self.rng = np.random.default_rng(seed)
+
+
+    def get_program(self, sketch_tokens: list[int], holes_tokens: list[list[int]]) -> list[int]:
+        # split sketch_tokens into multiple lists based on <HOLE> token
+        list_sketch = [[]]
+        for token in sketch_tokens:
+            if token == self.dsl.t2i['<HOLE>']:
+                list_sketch.append([])
+            else:
+                list_sketch[-1].append(token)
+        assert len(list_sketch) == len(holes_tokens) + 1
+        prog_tokens = list_sketch[0]
+        for i in range(len(holes_tokens)):
+            # As we removed invalid individuals, these asserts should not be necessary.
+            assert holes_tokens[i][0] == self.dsl.t2i['DEF']
+            assert holes_tokens[i][1] == self.dsl.t2i['run']
+            assert holes_tokens[i][2] == self.dsl.t2i['m(']
+            assert holes_tokens[i][-1] == self.dsl.t2i['m)']
+            prog_tokens += holes_tokens[i][3:-1] + list_sketch[i+1]
+        return prog_tokens
+    
+    
+    def remove_invalid_and_duplicates(self, latent_population: list[torch.Tensor],
+                                      decoded_population: list[list[list[int]]]):
+        new_latent_population, new_decoded_population = [], []
+        seen_programs = set()
+        for latent_level, decoded_level in zip(latent_population, decoded_population):
+            latent_level_list, decoded_level_list = [], []
+            for latent, decoded in zip(latent_level, decoded_level):
+                program_str = self.dsl.parse_int_to_str(decoded)
+                if program_str in seen_programs: continue
+                if program_str.startswith('DEF run m(') and program_str.endswith('m)'):
+                    seen_programs.add(program_str)
+                    latent_level_list.append(latent)
+                    decoded_level_list.append(decoded)
+            new_latent_population.append(latent_level_list)
+            new_decoded_population.append(decoded_level_list)
+        return new_latent_population, new_decoded_population
+    
+    
+    def argmax(self, q_values):
+        """
+        Takes in a list of q_values and returns the index of the item 
+        with the highest value. Breaks ties randomly.
+        returns: int - the index of the highest value in q_values
+        """
+        top_value = float("-inf")
+        ties = []
+        
+        for i in range(len(q_values)):
+            # if a value in q_values is greater than the highest value update top and reset ties to zero
+            # if a value is equal to top value add the index to ties
+            # return a random selection from ties.
+            if q_values[i] > top_value:
+                ties = [i]
+                top_value = q_values[i]
+            elif q_values[i] == top_value:
+                ties.append(i)
+        return self.rng.choice(ties)
+    
+    
+    def sample_programs(self, num_samples: int, decoded_population: list[list[list[int]]],
+                        q_values: list[list[float]]):
+        programs, population_indices = [], []
+        for _ in range(num_samples):
+            program = self.dsl.parse_str_to_int('DEF run m( <HOLE> m)')
+            this_population_indices = []
+            for level in range(len(self.models)):
+                n_holes = program.count(self.dsl.t2i['<HOLE>'])
+                holes, holes_indices = [], []
+                for _ in range(n_holes):
+                    # Epsilon-greedy selection
+                    if self.rng.random() < self.epsilon:
+                        hole_index = self.rng.choice(list(range(len(decoded_population[level]))))
+                    else:
+                        hole_index = self.argmax(q_values[level])
+                    holes_indices.append(hole_index)
+                    holes.append(decoded_population[level][hole_index])
+                try:
+                    program = self.get_program(program, holes)
+                except AssertionError:
+                    program = None
+                    break
+                this_population_indices.append(holes_indices)
+            # As invalid individuals have been removed, we should always have a valid program
+            assert program is not None
+            programs.append(program)
+            population_indices.append(this_population_indices)
+        return programs, population_indices
+    
+    
+    def step(self, decoded_population: list[list[list[int]]]):
+        q_values = [[0.0 for _ in range(len(pop))] for pop in decoded_population]
+        count_calls = [[0 for _ in range(len(pop))] for pop in decoded_population]
+        
+        if Config.multiprocessing_active: pool = Pool()
+        
+        for sample_iteration in range(1, self.sample_iterations + 1):
+            
+            # Sample programs from decoded population
+            programs, population_indices = self.sample_programs(self.batch_size, decoded_population, q_values)
+            
+            # Evaluate the programs
+            if Config.multiprocessing_active:
+                fn = partial(evaluate_program, task_envs=self.task_envs, dsl=self.dsl)
+                rewards = pool.map(fn, programs)
+            else:
+                rewards = [evaluate_program(program, self.task_envs, self.dsl) for program in programs]
+            self.num_eval += len(rewards)
+            StdoutLogger.log('Hierarchical Search', f'Sample iteration {sample_iteration} best reward: {max(rewards)}')
+            
+            # Update best program
+            if max(rewards) > self.best_reward:
+                self.best_reward = max(rewards)
+                self.best_program = self.dsl.parse_int_to_str(programs[np.argmax(rewards)])
+                StdoutLogger.log('Hierarchical Search', f'New best program: {self.best_program}')
+                StdoutLogger.log('Hierarchical Search', f'New best reward: {self.best_reward}')
+                StdoutLogger.log('Hierarchical Search', f'Number of evaluations: {self.num_eval}')
+                with open(self.output_file, mode='a') as f:
+                    t = time.time() - self.start_time
+                    f.write(f'{t},{self.num_eval},{self.best_reward},{self.best_program}')
+            
+            if self.best_reward >= 1.0:
+                self.converged = True
+                break
+            
+            # Update Q-values
+            for reward, indices in zip(rewards, population_indices):
+                for level in range(len(self.models)):
+                    for hole_index in indices[level]:
+                        count_calls[level][hole_index] += 1
+                        # Q-values here are estimates of max reward
+                        # if reward > q_values[level][hole_index]:
+                        q_values[level][hole_index] += (reward - q_values[level][hole_index]) / count_calls[level][hole_index]
+
+        if Config.multiprocessing_active: pool.close()
+        
+        return q_values
+    
+    
+    def search(self) -> tuple[str, bool, int]:
+        self.converged = False
+        self.num_eval = 0
+        self.best_reward = float('-inf')
+        self.best_program = None
+        self.start_time = time.time()
+        with open(self.output_file, mode='w') as f:
+            f.write('time,num_evaluations,best_reward,best_program\n')
+            
+        latent_population = [
+            torch.stack([
+                torch.randn(model.hidden_size, device=self.device)
+                for _ in range(pop_size)
+            ])
+            for model, pop_size in zip(self.models, self.pop_sizes)
+        ]
+        
+        decoded_population = [
+            model.decode_vector(pop)
+            for model, pop in zip(self.models, latent_population)
+        ]
+        
+        latent_population, decoded_population = self.remove_invalid_and_duplicates(latent_population, decoded_population)
+        
+        for search_iteration in range(1, self.search_iterations + 1):
+        
+            StdoutLogger.log('Hierarchical Search', f'Search iteration {search_iteration}.')
+            StdoutLogger.log('Hierarchical Search', f'Estimating Q-values.')
+        
+            # Estimate Q-values by executing sampled programs
+            q_values = self.step(decoded_population)
+            
+            if self.converged: break
+            
+            StdoutLogger.log('Hierarchical Search', f'Sampling next population.')
+            
+            # Get elite population based on Q-values
+            elite_population = []
+            for level in range(len(self.models)):
+                level_elite_population = []
+                n_elite = self.n_elite[level]
+                elite_indices = np.argpartition(q_values[level], -n_elite)[-n_elite:]
+                for index in elite_indices:
+                    level_elite_population.append(latent_population[level][index])
+                elite_population.append(torch.stack(level_elite_population))
+            
+            # Sample new population using CEM
+            for level in range(len(self.models)):
+                new_indices = torch.ones(elite_population[level].size(0), device=self.device).multinomial(
+                    self.pop_sizes[level], replacement=True)
+                new_population = []
+                for new_index in new_indices:
+                    sample = elite_population[level][new_index]
+                    new_population.append(sample + self.sigma * torch.randn_like(sample, device=self.device))
+                latent_population[level] = torch.stack(new_population)
+                decoded_population[level] = self.models[level].decode_vector(latent_population[level])
+            
+            latent_population, decoded_population = self.remove_invalid_and_duplicates(latent_population, decoded_population)
+
+        best_program_nodes = self.dsl.parse_str_to_node(self.best_program)
+        self.task_envs[0].trace_program(best_program_nodes, self.trace_file, 1000)
+        
+        return self.best_program, self.converged, self.num_eval
